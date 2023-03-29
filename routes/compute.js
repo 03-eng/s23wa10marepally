c54864fee77e512403a6d5ec171fd3a5094aecac
03-eng <teddyreddy789@gmail.com>
2023-03-29 01:23:01 -0500

    modified bonus

--- routes/compute.js
+++ routes/compute.js
@@ -2,31 +2,30 @@ var express = require('express');
 
 var router = express.Router();
 
-let x=Math.floor(Math.random());
+let x = Math.floor(Math.random());
 
-let y
+//let y
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-    if(req.query.x!=undefined){
+    if (req.query.x != undefined) {
 
-        x=req.query.x;
-
-
+        x = req.query.x;
 
 
-    }
 
-    let r=Math.log2(x);
+    }
+    let r = Math.fround(x);
+    let t = Math.random(x);
 
-    let t=Math.cosh(x);
+    let n = Math.round(x);
 
-    let n=Math.floor(x);
 
-   
 
-  res.render('compute', { x:x,log:r, cos:t, floor:n});
+    res.render('compute', { x: x, fround:r, random:t, round:n });
 
 });
+
+module.exports = router;
