30d26f3cdad6953519de05382afe649f4a34493c
03-eng <teddyreddy789@gmail.com>
2023-03-29 00:52:28 -0500

    added computation 2

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let x = Math.floor(Math.random());
-ley y
+let y
 /* GET home page.. */
-router.get('/',function(req,res,next){
+router.get('/',function(req, res, next) {
     if (req.query.x!=undefined)
     {
         x=req.query.x;
@@ -14,7 +14,6 @@ router.get('/',function(req,res,next){
     let k= Math.round(x);
 
 
-    res.render('computation', {x:x, fround:g, random:f, round:k});
-
+    res.render('computation', { x:x, fround:g, random:f, round:k});
 });
 module.exports=router;
